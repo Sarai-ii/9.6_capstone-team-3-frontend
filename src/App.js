d3260444bde4ebe35d4427b6232ac1dd39a6b970
JenniferEinstein <jennifereinstein@gmail.com>
2023-11-04 11:55:05 -0400

    First crack at pages, components, App, etc.

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+/* Import all page components here  */
+import Home from './pages/Home';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Heeader from './components/Header'
+import SignupLogin from '../pages/SignupLogin';
+import AllEvents from '../pages/AllEvents';
+import CurrentEvent from '../pages/CurrentEvent';
+import Gallery from '../pages/Gallery';
+import Profile from '../pages/Profile';
+import Account from '../pages/Account';
+import PageNotFound from "../pages/PageNotFound"; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={ <Home />} />
+            <Route path="/about" element = { <About /> } />
+            <Route path='/signup' element = { <SignupLogin /> } />
+            <Route path="/events" element = { <AllEvents /> } />
+            <Route path="/currentevent" element = { <CurrentEvent /> } />
+            <Route path="/gallery" element = { <Gallery /> } />
+            <Route path="/allevents" element = { <AllEvents /> } />
+            <Route path="/profile" element = { <Profile /> } />
+            <Route path="/account" element = { <Account /> } />
+            <Route path="*" element = { <PageNotFound /> } />
+          </Routes>
+        </main>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
