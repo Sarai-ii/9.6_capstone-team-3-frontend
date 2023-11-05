5932a1ffbff94ff565b20a99aa1fe0a37830bb30
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-11-05 16:01:57 -0500

    Front End Working

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 
+function Home() {
+  return (
+    <div>
+      <h1>Home Page for To:From:</h1>
+      
+      
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+export default Home;
