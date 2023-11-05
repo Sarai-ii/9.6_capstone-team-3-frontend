7ca26ed9b7d1e3213784fa73f0de727ea42e6f98
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-11-05 17:16:12 -0500

    Events changes so far, mapped through events and started working on css format with bootstrap. Pushing what I have so far.

--- src/pages/AllEvents.js
+++ src/pages/AllEvents.js
@@ -1,9 +1,33 @@
-import React from 'react'
+// DEPENDENCIES
+import axios from "axios";
+import { useState, useEffect } from "react";
+// COMPONENTS
+import Event from "./Event";
+// STYLING
+// import ""
+// API
+const API = process.env.REACT_APP_API_URL;
 
-function AllEvents() {
+export default function AllEvents() {
+  const [events, setEvents] = useState([])
+
+
+  useEffect(() => {
+    axios
+      .get(`${API}/events`)
+      .then((response) => setEvents(response.data))
+      .catch((c) => console.warn("catch", c));
+  }, []);
+
   return (
-    <div>AllEvents</div>
+    <div>
+      {events.map((event) => {
+          return <Event 
+          
+          key={event.id} 
+          event={event} 
+          />;
+        })}
+    </div>
   )
-}
-
-export default AllEvents
+};
