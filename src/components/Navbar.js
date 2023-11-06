93ad136d8b35d012eabcfb879e4d776d7760fd43
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-11-05 20:14:52 -0500

    some navbar styling, colors not final still playing with it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+import './Navbar.css';
+
+function NavBar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/ToFromLogo.png" alt="ToFromLogo" />
+      </div>
+      <div className='navbar-links'>
+        <Link className='navbar-login-link' to="/signup">Login</Link>
+        <Link className='navbar-events-link' to="/events">Events</Link> 
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default NavBar;
