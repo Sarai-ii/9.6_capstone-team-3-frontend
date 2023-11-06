8a2cbd9b5d32aabd7e20f1b26fcc587d701df924
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-11-05 20:45:08 -0500

    trying to ge the logo to work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,16 @@ import './Navbar.css';
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src="/ToFromLogo.png" alt="ToFromLogo" />
-      </div>
-      <div className='navbar-links'>
-        <Link className='navbar-login-link' to="/signup">Login</Link>
-        <Link className='navbar-events-link' to="/events">Events</Link> 
+      <Link to="/" className="navbar-logo">
+        <img src="/Logo.jpg" alt="ToFromLogo" />
+      </Link>
+      <div className="navbar-links">
+        <Link className="navbar-login-link" to="/signup">
+          Login
+        </Link>
+        <Link className="navbar-events-link" to="/events">
+          Events
+        </Link>
       </div>
     </nav>
   );
