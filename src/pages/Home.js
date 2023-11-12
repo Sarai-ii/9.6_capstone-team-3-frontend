a400ba90d4275fe751f336729a9a3a0820b1ad6b
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-11-12 16:29:38 -0500

    how it works updated with images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+import registrationImage from '../images/registration-image.png'
+import matchingImage from '../images/matching-image.png'
+import sendingGiftImage from '../images/send-a-gift-image.png'
+import receiveGiftImage from '../images/receiving-gift-image.png'
+
 import '../css/homepage.css';
 
 function Home() {
   return (
     <div className="homepage-container">
-      <div>
+      <div className='title-slogan-container'>
       <div className="h1-container">
         <h1 className="to-title">To:</h1>
         <h1 className="from-title">From:</h1>
@@ -39,22 +45,26 @@ function Home() {
   
 
           <div className="step-container">
-            <img src="/images/register.png" alt="Register" />
+            <h2 className='step-container-title'>Register</h2>
+            <img src={registrationImage} alt="Register" />
             <p>Register for an account to join our community.</p>
           </div>
 
           <div className="step-container">
-            <img src="/images/get-matched.png" alt="Get Matched" />
+          <h2 className='step-container-title'>Get Matched</h2>
+            <img src={matchingImage} alt="Get Matched" />
             <p>Get matched with someone in our community.</p>
           </div>
 
           <div className="step-container">
-            <img src="/images/send-gift.png" alt="Send a Gift" />
+          <h2 className='step-container-title'>Send a Gift</h2>
+            <img src={sendingGiftImage} alt="Send a Gift" />
             <p>Send a thoughtful gift to your match.</p>
           </div>
 
           <div className="step-container">
-            <img src="/images/receive-gift.png" alt="Receive a Gift" />
+          <h2 className='step-container-title'>Receive a Gift</h2>
+            <img src={receiveGiftImage} alt="Receive a Gift" />
             <p>Receive a thoughtful gift from your match.</p>
           </div>
         </section>
