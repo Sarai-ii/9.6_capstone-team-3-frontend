eb78cb0b9ff094a6b967174774d0db2812ee6244
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-11-12 14:38:59 -0500

    title-slogan-container created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import '../css/homepage.css';
 function Home() {
   return (
     <div className="homepage-container">
+      <div>
       <div className="h1-container">
         <h1 className="to-title">To:</h1>
         <h1 className="from-title">From:</h1>
@@ -14,6 +15,7 @@ function Home() {
         Bringing People Together Through Thoughtful Gestures
       </h2>
       <h3 className="slogan-2">Join the Exchange of Happiness</h3>
+      </div>
 
       <div className="signup-container">
         <section className="signup">
