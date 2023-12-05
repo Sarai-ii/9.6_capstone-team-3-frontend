e3ba788bdfe8a93558d068bae90cb719e536fb4e
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-12-04 22:01:07 -0500

    gave the submit page some CSS stlying, need to give label a unique ID so it doesnt clash with the rest of the page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,6 +3,9 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
 import axios from 'axios';
 
+import "../css/Signup.css"
+
+
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -73,101 +76,116 @@ const Signup = () => {
     
   
     return (
-      <div>
-        <h1>Create an account</h1>
-        <form onSubmit={handleSignup}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          First Name:
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <label>
-          Street Address 1:
-          <input
-            type="text"
-            value={addressStreet1}
-            onChange={(e) => setAddressStreet1(e.target.value)}
-          />
-        </label>
-        <label>
-          Street Address 2:
-          <input
-            type="text"
-            value={addressStreet2}
-            onChange={(e) => setAddressStreet2(e.target.value)}
-          />
-        </label>
-        <label>
-          City:
-          <input
-            type="text"
-            value={addressCity}
-            onChange={(e) => setAddressCity(e.target.value)}
-          />
-        </label>
-        <label>
-          State:
-          <input
-            type="text"
-            value={addressState}
-            onChange={(e) => setAddressState(e.target.value)}
-          />
-        </label>
-        <label>
-          ZIP Code:
-          <input
-            type="text"
-            value={addressZip}
-            onChange={(e) => setAddressZip(e.target.value)}
-          />
-        </label>
-        <label>
-          Bio:
-          <textarea value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
-        </label>
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? 'Signing up...' : 'Signup'}
-        </button>
-      </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
-    </div>
-  );
-};
+      <div id="signup-container">
+        <div id='signup-h1-container'>
+          <h1 id='signup-title-h1'>Create an account</h1>
+        </div>
+        <div id='signup-form-container' >
+          <form onSubmit={handleSignup} id="signup-form">
+            <label>
+              Email:
+              <input
+                id='signup-input-email'
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                id='signup-input-password'
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <label>
+              Username:
+              <input
+                id='signup-input-username'
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              First Name:
+              <input
+                id='signup-input-firstname'
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </label>
+            <label>
+              Last Name:
+              <input
+                id='signup-input-lastname'
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </label>
+            <label>
+              Street Address 1:
+              <input
+                id='signup-input-address1'
+                type="text"
+                value={addressStreet1}
+                onChange={(e) => setAddressStreet1(e.target.value)}
+              />
+            </label>
+            <label>
+              Street Address 2:
+              <input
+                id='signup-input-address2'
+                type="text"
+                value={addressStreet2}
+                onChange={(e) => setAddressStreet2(e.target.value)}
+              />
+            </label>
+            <label>
+              City:
+              <input
+                id='signup-input-city'
+                type="text"
+                value={addressCity}
+                onChange={(e) => setAddressCity(e.target.value)}
+              />
+            </label>
+            <label>
+              State:
+              <input
+               id='signup-input-state'
+                type="text"
+                value={addressState}
+                onChange={(e) => setAddressState(e.target.value)}
+              />
+            </label>
+            <label>
+              ZIP Code:
+              <input
+                id='signup-input-zip'
+                type="text"
+                value={addressZip}
+                onChange={(e) => setAddressZip(e.target.value)}
+              />
+            </label>
+            <label>
+              Bio:
+              <textarea id='signup-textarea-bio' value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
+            </label>
+            <button id='signup-button-submit' type="submit" disabled={isLoading}>
+              {isLoading ? 'Signing up...' : 'Signup'}
+            </button>
+          </form>
+        </div>
+        <div id='error-message-container'></div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+      </div>
+    );
+}
 
 export default Signup;
