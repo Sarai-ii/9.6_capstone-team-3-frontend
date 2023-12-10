9ff586f5fba7f1505fc57d458272adbab220c34a
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-12-10 10:10:02 -0500

    added on lcik to full galerry so that users can click image to reveal exchage info, made the h3 pink for that. also added filler text to first container, sentence is too long so will chage it to wrap around

--- src/components/FullGallery.js
+++ src/components/FullGallery.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-
 import '../css/fullGallery.css';
 
 function FullGallery() {
@@ -21,19 +20,31 @@ function FullGallery() {
     fetchPicturePosts();
   }, []);
 
+  const handleCardClick = (index) => {
+    const updatedPicturePosts = [...picturePosts];
+    updatedPicturePosts[index].showText = !updatedPicturePosts[index].showText;
+    setPicturePosts(updatedPicturePosts);
+  };
+
   return (
     <div className='picture-card-container'>
-        <div className='picture-card-h1-container'>
-            <Link className='picture-card-link' to="/gallery">
-      <h1 className='picture-card-h1'>Check Out Previous Exchanges</h1>
-            </Link>
-        </div>
-      {picturePosts.map((picturePost) => (
-        <div key={picturePost.id} className="picture-card">
+      <div className='picture-card-h1-container'>
+        <Link className='picture-card-link' to="/gallery">
+          <h1  id="picture-card-h1-title" className='picture-card-h1'>Check Out Previous Exchanges</h1>
+        </Link>
+      </div>
+      {picturePosts.map((picturePost, index) => (
+        <div
+          key={picturePost.id}
+          className={`picture-card ${picturePost.showText ? 'show-text' : ''}`}
+          onClick={() => handleCardClick(index)}
+        >
           <img className="picture-card-img" src={picturePost.pictures_post_url} alt={picturePost.pictures_post_title} />
-          <h3 className='picture-card-h3'>{picturePost.pictures_post_title}</h3>
-          <p>{picturePost.pictures_post_blurb}</p>
-          <p>Likes: {picturePost.likes_count}</p>
+          <div className="picture-card-overlay">
+            <h3 className='picture-card-h3'>{picturePost.pictures_post_title}</h3>
+            <p>{picturePost.pictures_post_blurb}</p>
+            <p>Likes: {picturePost.likes_count}</p>
+          </div>
         </div>
       ))}
     </div>
