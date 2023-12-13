f2fa3d6a6175f5c37ec3678408cebc4f8d01ba04
JenniferEinstein <jennifereinstein@gmail.com>
2023-12-13 13:59:11 -0500

    Wrote more but when creating a route created a problem. Committing to switch.

--- src/components/MessageMatch.js
+++ src/components/MessageMatch.js
@@ -1,6 +1,18 @@
+import React from 'react'
 
+function MessageMatch() {
+  return (
 <div className="message">
-    <p>Congratulations! You've been matched!
+    <h2>Congratulations! You've Been Matched for the Gift Exchange!</h2>
+    <p>Hi Mozzy, Exciting news! You've been successfully matched for the upcoming Gift Exchange. Now, the joy of gift-giving begins! To help you get started, here's some information about your recipient's preferences:</p>
+    <ul>
+        <li>Based on the theme, if given carte blanche, the person receiving your gift would like: a bottle of De Venoge Princes Tour Eiffel Champagne</li>
+        <li>The category they are most interested in is: food</li>
+        <li>Their favorite color is: cobalt blue</li> 
+        <li>They ask that you avoid: any meat</li>
+    </ul>
+    <p>
+
         You will be sending your gift to:
             17 Cherry Tree Lane
             Phoenix, AZ 85005
@@ -14,7 +26,20 @@
         <li></li>
         <li></li>
         <li></li>
-
     </ul>
 
+    Important Dates: Your thoughtful gift should be on its way by the SHIPPING DEADLINE on [insert date]. Once you've sent your gift, please click [HERE] to update us with the carrier, date, and tracking information.
+
+Sharing the Joy: When you receive your gift, don't forget to spread the joy! Upload a picture of your received gift to our gallery to confirm its safe arrival.
+
+We hope you have a fantastic time selecting and exchanging gifts. If you have any questions or need assistance, feel free to reach out.
+
+Happy Gifting!
+
+Best Regards, Happiness Exchange
+
 </div>
+  )
+}
+
+export default MessageMatch
