f494e008ff9fbcebcd513be4b64fd96de2639880
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-12-12 20:04:08 -0500

    login modal completed and styled only clsoing X will be changed later on

---
+++ src/components/LoginModal.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import "../css/LoginModal.css"
+
+const LoginModal = ({ openModal }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogin = async () => {
+    try {
+      if (!username || !password) {
+        console.error("Username and password are required.");
+        return;
+      }
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        username,
+        password
+      );
+      const user = userCredential.user;
+      const uid = user.uid;
+
+      setLoggedInUser(user);
+    } catch (error) {
+      console.error("Login Error:", error);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      setLoggedInUser(user);
+    } catch (error) {
+      console.error("Google Login Error:", error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setLoggedInUser(null);
+    } catch (error) {
+      console.error("Logout Error:", error);
+    }
+  };
+
+  return (
+    <div className="modal-container">
+      <div className="modal-content">
+        <span className="close" onClick={openModal}>
+          &times;
+        </span>
+
+        {loggedInUser ? (
+          <div>
+            <div id="login-forum-container" className="login-forum-container">
+              You are logged in as {loggedInUser.email}.
+              <button
+                id="logout"
+                className="button logout-button"
+                type="button"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="login-forum-container">
+            <div className="login-h3-container">
+              <h3 className="login-h3">Login</h3>
+            </div>
+            <form className="form-container">
+              <label className="username-label">
+                Email:
+                <input
+                  className="username-input"
+                  type="email"
+                  placeholder="Enter your email"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </label>
+              <label className="password-login">
+                Password:
+                <input
+                  className="password-input"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              <div className="button-container">
+                <button
+                  className="button"
+                  id="login-button"
+                  type="button"
+                  onClick={handleLogin}
+                >
+                  Login
+                </button>
+                <button
+                  id="google-login-button"
+                  className="google-btn"
+                  type="button"
+                  onClick={handleGoogleLogin}
+                >
+                  <div className="google-icon-wrapper"></div>
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/HELogo.png';
-
+import LoginModal from './LoginModal'; // Import the LoginModal component
 import '../css/Navbar.css';
 
 function Logo() {
@@ -9,27 +9,40 @@ function Logo() {
 }
 
 function NavBar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <nav className="navbar">
       <div className='navbar-middle-links-container'>
-      <Link to="/" className="navbar-logo">
-        <Logo />  
-      </Link>
-      <div className='navbar-middle-links'>
-  <Link to="/events" id="events-middle-link" className="navbar-middle-link">events</Link>
-  <Link to="/how-it-works" className="navbar-middle-link">how it works</Link>
-  <Link to="/about" className="navbar-middle-link">about</Link>
-</div>
+        <Link to="/" className="navbar-logo">
+          <Logo />  
+        </Link>
+        <div className='navbar-middle-links'>
+          <Link to="/events" id="events-middle-link" className="navbar-middle-link">events</Link>
+          <Link to="/how-it-works" className="navbar-middle-link">how it works</Link>
+          <Link to="/about" className="navbar-middle-link">about</Link>
+        </div>
       </div>
       <div className="navbar-links">
-        <Link className="navbar-login-link" to="/login">
+        {/* Replace the Login link with a button that opens the LoginModal */}
+        <button className="navbar-login-link" onClick={openModal}>
           Login 
-        </Link>
+        </button>
         <Link className="homepage-signup-link" to="/signup">
           Sign Up
         </Link> 
       </div>
+
+      {/* Render the modal if it's open */}
+      {isModalOpen && <LoginModal openModal={closeModal} />}
     </nav>
   );
 }
