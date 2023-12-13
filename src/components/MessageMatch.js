f8c1e80f1e87ddc387c3d052c130c9bdbadf1217
JenniferEinstein <jennifereinstein@gmail.com>
2023-12-13 12:49:00 -0500

    starting with rules

---
+++ src/components/MessageMatch.js
@@ -0,0 +1,20 @@
+
+<div className="message">
+    <p>Congratulations! You've been matched!
+        You will be sending your gift to:
+            17 Cherry Tree Lane
+            Phoenix, AZ 85005
+
+
+        Here are the rules:
+    </p>
+    <ul>
+        <li>Your gift must be mailed by </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+
+    </ul>
+
+</div>
