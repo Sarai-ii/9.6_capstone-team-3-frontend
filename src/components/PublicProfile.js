af06dca92f2d36d8ce7fff8d6426e116c3e0a246
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-12-27 16:23:23 -0500

    NewProfile layout for bio container

--- src/components/PublicProfile.js
+++ src/components/PublicProfile.js
@@ -57,11 +57,13 @@ export default function Profile({user, userData}) {
 
 
   return (
-    <div className='profile-container right-column'>
-      <section className='bio-container left-profile'>
-        <div className='profile-header'>
+    <div className='profile-container'>
+      <div className='profile-header'>
+        <img className='cover-pic' src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFjZWJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D"/>
+      </div>
+      <div className='profile-content'>
+        <section className='bio-container-left'>
           <div className='pfp-container'>
-            <img className='cover-pic' src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFjZWJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D"/>
             <Link to={"edit"}>
               <img className='pfp' 
               src={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png" }
@@ -70,37 +72,43 @@ export default function Profile({user, userData}) {
               />
             </Link>
           </div>
-        </div>
-        <p 
-        className='username'> 
-        {user.username}
-        <br />
-        { userData ?(
-          <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EDBB64" className="bi bi-clock-fill" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-            </svg> Joined 
-            <br />
+          <div className='bio-content'>
+            <p 
+            className='username'> 
+            {user.username}
+            </p>
           </div>
-        ) : ( <div>Hi</div>
-  
-        )}
-        <svg xmlns="http://www.w3.org/2000/svg" 
-        width="16" 
-        height="16" 
-        fill="#EDBB64" 
-        className="bi bi-people-fill" 
-        viewBox="0 0 16 16">
-          <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
-        </svg> 0 Following/0 Followers
-        </p>
-        {/* <div className="logo-container">
-          <img className="insta logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"/>
-          <img className="fb logo" src='https://pngimg.com/uploads/facebook_logos/small/facebook_logos_PNG19757.png'/>
-          <img className="twitter logo" src="https://seeklogo.com/images/T/twitter-new-logo-8A0C4E0C58-seeklogo.com.png?v=638258088440000000"/>
-        </div> */}
-      </section>
-        <main className='posts-container right-profile'>
+          <p>
+          { userData ?(
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EDBB64" className="bi bi-clock-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+              </svg> Joined 
+              <br />
+            </div>
+          ) : ( 
+          <p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EDBB64" className="bi bi-clock-fill" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+            </svg> Loading 
+          </p>)}
+          
+          <svg xmlns="http://www.w3.org/2000/svg" 
+          width="16" 
+          height="16" 
+          fill="#EDBB64" 
+          className="bi bi-people-fill" 
+          viewBox="0 0 16 16">
+            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+          </svg> 0 Following/0 Followers
+          </p>
+            {/* <div className="logo-container">
+              <img className="insta logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"/>
+              <img className="fb logo" src='https://pngimg.com/uploads/facebook_logos/small/facebook_logos_PNG19757.png'/>
+              <img className="twitter logo" src="https://seeklogo.com/images/T/twitter-new-logo-8A0C4E0C58-seeklogo.com.png?v=638258088440000000"/>
+            </div> */}
+        </section>
+        <main className='posts-container-right'>
           <div className="posts-headings">
           <h2 lassName='name'>Welcome, {user.name_first}</h2>
           </div>
@@ -145,6 +153,7 @@ export default function Profile({user, userData}) {
           <h2>FillerText</h2>
         </main>
       </div>
+    </div>
   )
 }
 
