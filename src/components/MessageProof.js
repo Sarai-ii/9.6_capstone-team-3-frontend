56dd264ba43734bb7a8de16022795e73c3a6abd5
JenniferEinstein <jennifereinstein@gmail.com>
2024-01-09 16:20:00 -0500

    Moved changes from add-title-etc-upload=modal-jme now that JP is done working on this component.

--- src/components/MessageProof.js
+++ src/components/MessageProof.js
@@ -38,43 +38,40 @@ function MessageProof() {
 
 
   return (
-<div className="message">
-    {/* <h1>logo here?</h1> */}
-    <h2>Time to add pictures of the gift you received to the gallery!</h2>
-    <p>
-        Take a picture and upload it to the gallery {' '}
-        <button className='button' onClick={openUploadModal}>here</button>. Adding your picture lets us know that your match did their job. You will be given an opportunity to leave a little note describing your gift and/or thoughts. Our aim is to spread joy! 
-    </p>
-    <p>
-        Please don't leave a negative note. 
-    </p>
-    <p>
-        Even if you are disappointed in your gift, your match went out of their way to think about what you might like and sent it to you. If you really can't think of anything nice to say, saying "Thanks for the gift" is perfectly accceptable. 
-    </p>
-    <p>
-        The one exception is if you received something inappropriate or against the rules/law. In that case, don't upload the picture and let us know immediately at ResponseTeam@HappinessExchange.com.
-    </p>
-
-    <p>
-        We look forward to seeing you at our next event.
-    </p>
-    <p>Best Regards, 
-      <p>Happiness Exchange</p>    
-    </p>
-
-      {/* Modal */}
-      <UploadModal
-  isOpen={isModalOpen}
-  onRequestClose={closeUploadModal}
-  onUpload={(event) => handleUpload(event)} // Ensure to pass the event object
-/>
-     {uploadSuccess && (
-        <div>
-          <h2>Thank You!</h2>
-          <p>Your upload was successful. Thank you for sharing!</p>
+    <div className="message">
+        {/* <h1>logo here?</h1> */}
+        <h2>Time to add a picture of the gift you received to the gallery!</h2>
+        <p> Adding your picture lets us know that your match did their job. You will be given an opportunity to leave a little note describing your gift and/or thoughts. Our aim is to spread joy!</p>
+        
+        <p>Please don't leave a negative note. </p>
+        
+        <p>
+            Even if you are disappointed in your gift, your match went out of their way to think about what you might like and sent it to you. If you really can't think of anything nice to say, saying "Thanks for the gift" is perfectly accceptable. 
+        </p>
+        <p>
+            The one exception is if you received something inappropriate or against the rules. In that case, don't upload the picture and do let us know immediately at ResponseTeam@HappinessExchange.com.
+        </p>
+        <div id='upload-modal-button-in-message'>
+          {/* Modal */}
+            <UploadModal
+              isOpen={isModalOpen}
+              onRequestClose={closeUploadModal}
+              onUpload={(event) => handleUpload(event)} // Ensure to pass the event object
+            />
+          {uploadSuccess && (
+              <div>
+                <h2>Thank You!</h2>
+                <p>Your upload was successful. Thank you for sharing!</p>
+              </div>
+            )}
         </div>
-      )}
-</div>
+
+        <p>We look forward to seeing you at our next event.</p>
+
+        <p>Best Regards,</p>
+        
+        <p>Happiness Exchange</p>    
+    </div>
   )
 }
 
